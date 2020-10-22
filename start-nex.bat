5c14a64deb48132a170aae7325486c535a806875
Liuxj <liuxiaojie@nexnovo.com>
2020-10-22 17:43:32 +0800

    ?

--- start-nex.bat
+++ start-nex.bat
@@ -9,7 +9,7 @@ echo 复制所有: %_isCopyAll%
 pause
 
 if "%_isCopyAll%" == "y" (
-	cp -r F:/nexnovo/nexposter/src/assets/i18n/*.json i18n-nex/	
+	cp -r F:/nexnovo/nex-poster-ionic3/src/assets/i18n/*.json i18n-nex/	
 	goto start
 ) else (
 	goto copyZh
@@ -24,14 +24,14 @@ set /p _copy="要复制NexPoster中的zh.json文件过来吗? (y/n)"
 echo 复制zh.json: %_copy%
 pause
 if "%_copy%" == "y" (
-	cp F:/nexnovo/nexposter/src/assets/i18n/zh.json i18n-nex/
+	cp F:/nexnovo/nex-poster-ionic3/src/assets/i18n/zh.json i18n-nex/
 )
 goto start
 exit
 
 
 :start
-start explorer.exe /e,/root,F:\nexnovo\nexposter\src\assets\i18n\
+start explorer.exe /e,/root,F:\nexnovo\nex-poster-ionic3\src\assets\i18n\
 npm run start-nex
 pause
 exit
